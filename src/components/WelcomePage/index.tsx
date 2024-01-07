9e768723b534d9271070de1d9160ce09e3dc2cf4
Despoina <dntakou@core-innovation.com>
2024-01-07 17:27:29 +0200

    minor changes

--- src/components/WelcomePage/index.tsx
+++ src/components/WelcomePage/index.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, Typography, Select, MenuItem, OutlinedInput } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  Select,
+  MenuItem,
+  OutlinedInput,
+} from "@mui/material";
 import React, { useState } from "react";
 import { styles } from "./styles";
 
@@ -11,20 +18,21 @@ export default function WelcomePage() {
 
   return (
     <Box sx={styles.parentContainer}>
-      <Typography
-        variant="h3"
-        component="h3"
-        sx={{ textAlign: "center", color: "#000000", marginTop: "2rem" }}
-      >
-        CORE'S NLP APP
-      </Typography>
+      <Box sx={styles.typographyContainer}>
+        <Typography variant="h3" component="h3" sx={styles.typography}>
+          CORE'S NLP APP
+        </Typography>
+      </Box>
 
       <Box sx={styles.mainContainer}>
+        <Typography variant="h5" component="h5" sx = { styles.paragraph}>
+          Welcome to CORE's NLP App. Please select your project!
+        </Typography>
         <Select
           label="Please select your Project"
           value={selectedProject}
           onChange={chooseProject}
-          input={<OutlinedInput  label ="please select a projext" />}
+          input={<OutlinedInput label="please select a projext" />}
         >
           <MenuItem value="project1">Project1</MenuItem>
           <MenuItem value="project2">Project2</MenuItem>
--- src/components/WelcomePage/styles.ts
+++ src/components/WelcomePage/styles.ts
@@ -1,4 +1,23 @@
 export const styles = {
-  parentContainer: {},
+  parentContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   mainContainer: {},
+  typographyContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "15vh",
+    width: "100%",
+    backgroundColor: "#c9c4c3",
+  },
+  typography: {
+    textAlign: "center",
+  },
+  paragraph: {
+    marginTop: "10vh"
+  }
 };
