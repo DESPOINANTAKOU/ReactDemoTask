df44b5f019d6880efc88b04cb20c3bee7743f4e5
Despoina <dntakou@core-innovation.com>
2024-01-08 18:53:58 +0200

    changes in the select

--- src/components/SelectComponent/index.tsx
+++ src/components/SelectComponent/index.tsx
@@ -16,18 +16,7 @@ const MenuProps = {
   },
 };
 
-const names = [
-  'Oliver Hansen',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
-];
+const projects = ["Argus", "Project2", "Project3", "Project4"];
 
 function getStyles(name: string, personName: readonly string[], theme: Theme) {
   return {
@@ -56,7 +45,6 @@ export default function MySelect() {
     <div>
       <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
         <Select
-          multiple
           displayEmpty
           value={personName}
           onChange={handleChange}
@@ -74,11 +62,11 @@ export default function MySelect() {
           <MenuItem disabled value="">
             <em>Placeholder</em>
           </MenuItem>
-          {names.map((name) => (
+          {projects.map((project) => (
             <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, personName, theme)}
+              key={project}
+              value={project}
+              style={getStyles(project, personName, theme)}
             >
               {project}
             </MenuItem>
