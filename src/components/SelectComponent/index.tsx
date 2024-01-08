b6ea2d5c43392707d35d5bdf9032690f9f72dbaf
despoina <dntakou@upgrade.net.gr>
2024-01-08 14:35:10 +0200

    xcvbvgfd

--- src/components/SelectComponent/index.tsx
+++ src/components/SelectComponent/index.tsx
@@ -1,9 +1,9 @@
-import * as React from 'react';
-import { Theme, useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import * as React from "react";
+import { Theme, useTheme } from "@mui/material/styles";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -16,18 +16,7 @@ const MenuProps = {
   },
 };
 
-const names = [
-  'Oliver Hansen',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
-];
+const projects = ["Argus", "Project2", "Project3", "Project4"];
 
 function getStyles(name: string, personName: readonly string[], theme: Theme) {
   return {
@@ -48,7 +37,7 @@ export default function MySelect() {
     } = event;
     setPersonName(
       // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
+      typeof value === "string" ? value.split(",") : value
     );
   };
 
@@ -56,7 +45,6 @@ export default function MySelect() {
     <div>
       <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
         <Select
-          multiple
           displayEmpty
           value={personName}
           onChange={handleChange}
@@ -66,21 +54,21 @@ export default function MySelect() {
               return <em>Placeholder</em>;
             }
 
-            return selected.join(', ');
+            return selected.join(", ");
           }}
           MenuProps={MenuProps}
-          inputProps={{ 'aria-label': 'Without label' }}
+          inputProps={{ "aria-label": "Without label" }}
         >
           <MenuItem disabled value="">
             <em>Placeholder</em>
           </MenuItem>
-          {names.map((name) => (
+          {projects.map((project) => (
             <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, personName, theme)}
+              key={project}
+              value={project}
+              style={getStyles(project, personName, theme)}
             >
-              {name}
+              {project}
             </MenuItem>
           ))}
         </Select>
